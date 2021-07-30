d346d7eb6030e85e810afc4399d196707fcb3310
jinn2u <bsybear623@gmail.com>
2021-07-30 15:58:14 +0900

    Observer 패턴 구현해보기

---
+++ Observer/src/core/observer.js
@@ -0,0 +1,33 @@
+let currentObserver = null;
+
+export const observe = fn => {
+  currentObserver = fn;
+  fn();
+  currentObserver = null;
+}
+
+export const observable = obj => {
+  Object.keys(obj).forEach(key => {
+    let _value = obj[key];
+    const observers = new Set();
+    Object.defineProperty(obj, key, {
+      get () {
+        if (currentObserver) {
+          observers.add(currentObserver);
+          console.log(Array.from(observers).length)
+        } else {
+          console.log("is null?")
+        }
+          
+        return _value;
+      },
+      set (value) {
+        console.log("setter call")
+        _value = value;
+        observers.forEach(fn => fn());
+      }
+    })
+  })
+  return obj;
+}
+
